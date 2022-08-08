5e37640a0aecee8489b73630b4ddf8139079e9ed
Andre Luiz <andre1981luiz@gmail.com>
2022-08-07 22:01:56 -0300

    Commit Inicial - Criacao de Projeto

---
+++ src/main/java/com/Asilva01/impactamongodb/ImpactamongodbApplication.java
@@ -0,0 +1,13 @@
+package com.Asilva01.impactamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactamongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactamongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Asilva01/impactamongodb/ImpactamongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Asilva01.impactamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactamongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
